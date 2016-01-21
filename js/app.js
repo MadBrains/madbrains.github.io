4a47ce73cd21a5b96aaebae3f66444e9d8334561
i.ushakov <i.ushakov@perpetuumlab.com>
2016-01-21 15:07:39 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-jQuery(function() {
-  jQuery.scrollDepth();
-});
+(function ($) {
+
+    $(function () {
+
+		jQuery.scrollDepth();
+
+		$('.scrolly').click(function(e){
+			e.preventDefault();
+			$('#projects').animatescroll({scrollSpeed:500});
+		});
+
+		$('#header').scrollex({
+    		enter: function() {
+        		$('.more').css('visibility', 'visible');
+
+   			},
+    		leave: function() {
+        		$('.more').css('visibility', 'hidden');
+    		}
+  		});
+
+	});
+
+})(jQuery);
